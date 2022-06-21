b22ef2767121e579ace007aeec08795987e920db
Schalk1e <schalk@praekelt.org>
2022-06-21 21:25:06 +0200

    Simplify. (@Schalk1e)

--- cmdutil/ansi.go
+++ cmdutil/ansi.go
@@ -1,50 +1,10 @@
 package cmdutil
 
-func Header() (ansicode string) {
-	ansicode = "\033[95m"
-
-	return
-}
-
-func Blue() (ansicode string) {
-	ansicode = "\033[94m"
-
-	return
-}
-
-func Cyan() (ansicode string) {
-	ansicode = "\033[96m"
-
-	return
-}
-
-func Green() (ansicode string) {
-	ansicode = "\033[92m"
-
-	return
-
-}
-
-func Red() (ansicode string) {
-	ansicode = "\033[31m"
-
-	return
-}
-
-func Bold() (ansicode string) {
-	ansicode = "\033[1m"
-
-	return
-}
-
-func Underline() (ansicode string) {
-	ansicode = "\033[4m"
-
-	return
-}
-
-func End() (ansicode string) {
-	ansicode = "\033[0m"
-
-	return
-}
+const (
+	blue  = "\033[94m"
+	cyan  = "\033[96m"
+	green = "\033[92m"
+	red   = "\033[31m"
+	bold  = "\033[1m"
+	end   = "\033[0m"
+)
--- cmdutil/format.go
+++ cmdutil/format.go
@@ -5,29 +5,16 @@ import (
 )
 
 func PrintDiagnoseSuccess(msg string) {
-	var bold = Bold()
-	var green = Green()
-	var cyan = Cyan()
-	var end = End()
-
 	fmt.Println("\n" + bold + cyan + "Done: " + end + msg)
 	fmt.Println("╰-" + green + "✔ Success" + end)
 }
 
 func PrintDiagnoseFail(msg string) {
-	var bold = Bold()
-	var cyan = Cyan()
-	var red = Red()
-	var end = End()
-
 	fmt.Println("\n" + bold + cyan + "Done: " + end + msg)
 	fmt.Println("╰-" + red + "✘ Failed" + end)
 }
 
 func PrintArrows(msgs []string) {
-	var blue = Cyan()
-	var end = End()
-
 	for i, msg := range msgs {
 		if i == 0 {
 			fmt.Println("\n" + blue + "⮕ " + end + msg)
@@ -38,23 +25,13 @@ func PrintArrows(msgs []string) {
 }
 
 func PrintMoved() {
-	var green = Green()
-	var end = End()
-
 	fmt.Println("╰-" + green + "✔ Moved" + end)
 }
 
 func PrintDeleted() {
-	var green = Green()
-	var end = End()
-
 	fmt.Println("╰-" + green + "✔ Removed" + end)
 }
 
 func PrintWarning(msg string) {
-	var red = Red()
-	var bold = Bold()
-	var end = End()
-
 	fmt.Println("\n" + bold + red + "‼" + end + " " + msg)
 }
